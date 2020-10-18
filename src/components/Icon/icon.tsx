2cfda06e5dc9b4ccc843d486b77ba1ec5cca36da
mctl <musicatravelove@gmail.com>
2020-10-18 20:33:27 +0800

    icon修改

--- src/components/Icon/icon.tsx
+++ src/components/Icon/icon.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library, IconProp } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
@@ -13,11 +13,16 @@ export interface IconProps extends FontAwesomeIconProps{
     /**
      * 图标风格
      */
-    theme?: ThemeProps
+    theme?: ThemeProps;
+    /**
+     * 图标
+     */
+    icon: IconProp;
 }
 
 /**
- 封装了fortawesome插件，可使用以下图标
+ 封装了fortawesome插件，可在这里查看所有支持的图标:https://fontawesome.com/icons?d=gallery&m=free
+ <br>以下为部分举例：
  */
 const Icon: React.FC<IconProps> = (props) => {
     const { className, theme, ...restProps } = props

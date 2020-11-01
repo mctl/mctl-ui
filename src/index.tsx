4e739b657b21a1fbfce8fac74e0bd440a3475524
mctl <musicatravelove@gmail.com>
2020-11-01 15:19:35 +0800

    导出打包

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export { default as Button } from './components/button'
+export { default as Icon } from './components/icon'
+export { default as Input } from './components/input'
+export { default as Menu } from './components/menu'
+export { default as Transition } from './components/transition'
